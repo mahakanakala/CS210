d462993eab98097b54968213678f38714be883be
mahakanakala <maha.kanakala@gmail.com>
2024-04-24 21:39:11 -0400

    remove id fields for all tables

--- hw/hw4/schema.sql
+++ hw/hw4/schema.sql
@@ -8,7 +8,7 @@ You are allowed up to 3 submissions, only the last submission will be graded.
 
 NOTE: You may populate the tables with data for your own testing, but we do not want you to turn in any of the data, or the results of any of your queries. We are only asking for the document with the required SQL statements for table creation and queries.
 
-
+Database Schema (50 pts)
 You are given the following description of the entities that need to be stored in the database. Your task is to design a database schema (set of tables) to store these entities.
 
 Your schema must be minimally redundant in storing data. In other words, you should build a set of tables that minimize the repetition of data, by using foreign keys - credit will be in accordance with this.
@@ -34,7 +34,7 @@ For size of data, think of a realistic online music service and imagine how many
 Make sure you define and specify all primary keys, foreign keys, unique valued columns or unique valued combination of columns, and null/non-null properties for columns.
 
 In the document you will submit, type in the create table statement for each of the tables you create in the database. If you don't have the full create statement for a table, you will not get credit for it.
-Note: When you test your design in MySQL, you might use alter table statements after the initial create. However, for the submission, you are required to rewrite the whole sequence as a single create table statement per table.
+Note: When you test your design in MySQL, you might use alter table statements after the initial create. However, for the submission, you are required to rewrite the whole sequence as a single create table statement per table
 
 */
 
@@ -42,57 +42,53 @@ CREATE DATABASE DB;
 USE DB;
 
 CREATE TABLE artist(
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(50) NOT NULL UNIQUE
+    `artist_name` VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE 
 );
 
 CREATE TABLE album(
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `title` VARCHAR(50) NOT NULL,
+    `album_name` VARCHAR(50) NOT NULL,
     `release_date` DATETIME,
-    `artist_id` INT,
-    FOREIGN KEY (`artist_id`) REFERENCES artist(`id`)
+    `artist_name` VARCHAR(50),
+    PRIMARY KEY (`album_name`, `artist_name`),
+    FOREIGN KEY (`artist_name`) REFERENCES artist(`name`)
 );
 
 CREATE TABLE song(
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `title` VARCHAR(50) NOT NULL,
-    `artist_id` INT,
-    `album_id` INT,
-    FOREIGN KEY (`artist_id`) REFERENCES artist(`id`),
-    FOREIGN KEY (`album_id`) REFERENCES album(`id`)
+    `song_title` VARCHAR(50) NOT NULL,
+    `artist_name` VARCHAR(50) NOT NULL,
+    `album_title` VARCHAR(50),
+    PRIMARY KEY (`song_title`, `artist_name`),
+    FOREIGN KEY (`artist_name`) REFERENCES artist(`name`),
+    FOREIGN KEY (`album_title`) REFERENCES album(`album_title`)
 );
 
 CREATE TABLE user(
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(50) NOT NULL UNIQUE
+    `username` VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE
 );
 
 CREATE TABLE playlist(
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `title` VARCHAR(50) NOT NULL,
-    `time_created` DATETIME,
-    `user_id` INT,
-    FOREIGN KEY (`user_id`) REFERENCES user(`id`)
+    `playlist_title` VARCHAR(50) NOT NULL,
+    `time_created` DATETIME NOT NULL,
+    `username` VARCHAR(50),
+    PRIMARY KEY (`playlist_title`, `time_created`),
+    FOREIGN KEY (`username`) REFERENCES user(`username`)
 );
 
 CREATE TABLE playlist_song(
-    `playlist_id` INT,
-    `song_id` INT,
-    PRIMARY KEY (`playlist_id`, `song_id`),
-    FOREIGN KEY (`playlist_id`) REFERENCES playlist(`id`),
-    FOREIGN KEY (`song_id`) REFERENCES song(`id`)
+    `playlist_title` VARCHAR(50) NOT NULL,
+    `song_title` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`playlist_title`, `song_title`),
+    FOREIGN KEY (`playlist_title`) REFERENCES playlist(`playlist_title`),
+    FOREIGN KEY (`song_title`) REFERENCES song(`song_title`)
 );
 
 CREATE TABLE rating(
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT,
-    `song_id` INT,
-    `album_id` INT,
-    `playlist_id` INT,
+    `username` VARCHAR(50) NOT NULL,
+    `song_title` VARCHAR(50) NOT NULL,
+    `album_title` VARCHAR(50) NOT NULL,
     `rating` INT CHECK (rating >= 1 AND rating <= 5), 
-    FOREIGN KEY (`user_id`) REFERENCES user(`id`),
-    FOREIGN KEY (`song_id`) REFERENCES song(`id`),
-    FOREIGN KEY (`album_id`) REFERENCES album(`id`),
-    FOREIGN KEY (`playlist_id`) REFERENCES playlist(`id`)
+    PRIMARY KEY (`username`, `song_title`, `album_title`),
+    FOREIGN KEY (`username`) REFERENCES user(`username`),
+    FOREIGN KEY (`song_title`) REFERENCES song(`song_title`),
+    FOREIGN KEY (`album_title`) REFERENCES album(`album_title`)
 );
